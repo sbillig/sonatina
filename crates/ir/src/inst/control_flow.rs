9c560c06acd627cc6703ea083e9537a04c4944bd
Sean Billig <sean.billig@gmail.com>
2024-11-23 19:56:02 -0800

    wip update old code for new insts

--- crates/ir/src/inst/control_flow.rs
+++ crates/ir/src/inst/control_flow.rs
@@ -1,5 +1,5 @@
 use macros::{inst_prop, Inst};
-use smallvec::SmallVec;
+use smallvec::{smallvec, SmallVec};
 
 use crate::{module::FuncRef, BlockId, Inst, InstSetBase, ValueId};
 
@@ -36,10 +36,6 @@ pub struct Phi {
 }
 
 impl Phi {
-    pub fn iter_args(&self) -> impl Iterator<Item = &(ValueId, BlockId)> {
-        self.args.iter()
-    }
-
     pub fn append_phi_arg(&mut self, value: ValueId, block: BlockId) {
         self.args.push((value, block))
     }
@@ -81,7 +77,7 @@ pub struct Return {
 
 #[inst_prop]
 pub trait Branch {
-    fn dests(&self) -> Vec<BlockId>;
+    fn dests(&self) -> SmallVec<[BlockId; 2]>;
     fn num_dests(&self) -> usize;
     fn remove_dest(&self, isb: &dyn InstSetBase, dest: BlockId) -> Box<dyn Inst>;
     fn rewrite_dest(&self, isb: &dyn InstSetBase, from: BlockId, to: BlockId) -> Box<dyn Inst>;
@@ -91,8 +87,8 @@ pub trait Branch {
 }
 
 impl Branch for Jump {
-    fn dests(&self) -> Vec<BlockId> {
-        vec![self.dest]
+    fn dests(&self) -> SmallVec<[BlockId; 2]> {
+        smallvec![self.dest]
     }
 
     fn num_dests(&self) -> usize {
@@ -118,8 +114,8 @@ impl Branch for Jump {
 }
 
 impl Branch for Br {
-    fn dests(&self) -> Vec<BlockId> {
-        vec![self.nz_dest, self.z_dest]
+    fn dests(&self) -> SmallVec<[BlockId; 2]> {
+        smallvec![self.nz_dest, self.z_dest]
     }
 
     fn num_dests(&self) -> usize {
@@ -153,11 +149,11 @@ impl Branch for Br {
 }
 
 impl Branch for BrTable {
-    fn dests(&self) -> Vec<BlockId> {
+    fn dests(&self) -> SmallVec<[BlockId; 2]> {
         let mut dests = if let Some(default) = self.default {
-            vec![default]
+            smallvec![default]
         } else {
-            vec![]
+            smallvec![]
         };
         dests.extend(self.table.iter().map(|(_, block)| *block));
 
